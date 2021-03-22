4dd1106d2a151169d974b1708ea7a6cbab0487ed
dron5 <dron5@mail.ru>
2021-03-22 10:32:43 +0700

    save4

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-multi-str */
 
 import React from "react";
+
+import Tags from "../Tags/Tags";
 import classes from "./ArticlePreview.module.scss";
 
 import avatar from "../../img/avatar.png";
@@ -9,18 +11,21 @@ const ArticlePreview = () =>{
   const text = "Lorem ipsum dolor sit amet, \
     consectetur adipiscing elit, sed do eiusmod tempor \
     incididunt ut labore et dolore magna aliqua. \
-     Ut enim ad minim veniam, quis nostrud exercitation \
-     ullamco laboris  nisi ut aliquip ex ea commodo consequat."; 
+    Ut enim ad minim veniam, quis nostrud exercitation \
+    ullamco laboris  nisi ut aliquip ex ea commodo consequat. \
+    Lorem ipsum dolor sit amet, \
+    consectetur adipiscing elit, sed do eiusmod tempor \
+    incididunt ut labore et dolore magna aliqua. "; 
 return (
   <div className={classes.preview}>
     <div className={classes["preview__short-desc"]}>
-      <div className={classes.preview__title}>title</div>
-      <div className={classes.preview__tags}>tags</div>
+      <div className={classes.preview__title}> Article preview title</div>
+      <Tags />
       <div className={classes.preview__description}>{text}</div>
     </div>
     <div className={classes["preview__user-info"]}>
       <div className={classes.preview__name}>
-        <span>name</span>
+        <span>Jone Doe</span>
         <span>data</span>
       </div>
       <img src={avatar} alt="avatar" />
---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-array-index-key */
+import React from "react";
+import classes from "./Tags.module.scss";
+
+const Tags = () =>{
+  const tagList = ['gulag', 'hype', 'GM'];
+  const part = tagList.map((el, i) => (
+    <button key={i} type="button" className={classes.preview__tags}>
+      {el}
+    </button>
+  ));
+return (
+    <div>{part}</div>
+  );
+};
+
+export default Tags;
