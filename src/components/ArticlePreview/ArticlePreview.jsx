a8712fa23d13ce907444b82e1cb1276b24bab896
dron5 <dron5@mail.ru>
2021-03-22 10:45:20 +0700

    Date added

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -1,36 +1,39 @@
 /* eslint-disable no-multi-str */
 
 import React from "react";
+import { format } from "date-fns";
 
 import Tags from "../Tags/Tags";
 import classes from "./ArticlePreview.module.scss";
 
 import avatar from "../../img/avatar.png";
 
-const ArticlePreview = () =>{
-  const text = "Lorem ipsum dolor sit amet, \
+const ArticlePreview = () => {
+  const date = new Date();
+  const text =
+    "Lorem ipsum dolor sit amet, \
     consectetur adipiscing elit, sed do eiusmod tempor \
     incididunt ut labore et dolore magna aliqua. \
     Ut enim ad minim veniam, quis nostrud exercitation \
     ullamco laboris  nisi ut aliquip ex ea commodo consequat. \
     Lorem ipsum dolor sit amet, \
     consectetur adipiscing elit, sed do eiusmod tempor \
-    incididunt ut labore et dolore magna aliqua. "; 
-return (
-  <div className={classes.preview}>
-    <div className={classes["preview__short-desc"]}>
-      <div className={classes.preview__title}> Article preview title</div>
-      <Tags />
-      <div className={classes.preview__description}>{text}</div>
-    </div>
-    <div className={classes["preview__user-info"]}>
-      <div className={classes.preview__name}>
-        <span>Jone Doe</span>
-        <span>data</span>
+    incididunt ut labore et dolore magna aliqua. ";
+  return (
+    <div className={classes.preview}>
+      <div className={classes["preview__short-desc"]}>
+        <div className={classes.preview__title}> Article preview title</div>
+        <Tags />
+        <div className={classes.preview__description}>{text}</div>
       </div>
-      <img src={avatar} alt="avatar" />
+      <div className={classes["preview__user-info"]}>
+        <div className={classes.preview__name}>
+          <span>Jone Doe</span>
+          <span>{format(new Date(date), 'PP')} </span>
+        </div>
+        <img src={avatar} alt="avatar" />
+      </div>
     </div>
-  </div>
-);
+  );
 };
 export default ArticlePreview;
--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -2,16 +2,14 @@
 import React from "react";
 import classes from "./Tags.module.scss";
 
-const Tags = () =>{
-  const tagList = ['gulag', 'hype', 'GM'];
+const Tags = () => {
+  const tagList = ["gulag", "hype", "GM"];
   const part = tagList.map((el, i) => (
     <button key={i} type="button" className={classes.preview__tags}>
       {el}
     </button>
   ));
-return (
-    <div>{part}</div>
-  );
+  return <div>{part}</div>;
 };
 
 export default Tags;
