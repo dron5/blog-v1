004e67ad0613ebe2f049bab4999a7d9dfd025a5a
dron5 <dron5@mail.ru>
2021-03-30 17:10:11 +0700

    save

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,16 +1,45 @@
 import React from "react";
-
-import { Button } from "antd";
+import { Link } from "react-router-dom";
+import {useForm} from "react-hook-form";
 
 import classes from "./SignIn.module.scss";
 
-const SignIn = () => (
-  <div className={classes.menu__signin}>
-    <span className={classes.menu__title}>Sign In account</span>
-    <Button type="submit" className={classes["ant-btn"]}>
-      Create
-    </Button>
-  </div>
-);
-
+const SignIn = () => {
+  const {register, handleSubmit, errors} = useForm();
+  console.log('errors', errors);
+  const onSubmit = (data) => console.log('data', data);
+  return (
+    <div className={classes.container}>
+      <span className={classes.menu__title}>Sign In account</span>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={classes["form-group"]}>
+          <input 
+            ref={register} type="email" name="email" 
+            className={classes["form-control"]}
+            placeholder=" " id="email" />
+          <label
+            htmlFor="email"
+            className={classes["form-label"]}
+            >
+            <span>Email address</span>
+            </label>
+        </div>
+        <div className={classes["form-group"]}>
+          <input ref={register({required: true, minLength: 8, maxLength: 20})}
+           type="password" name="password" className={classes["form-control"]}
+           placeholder=" " id="password" />
+          <label htmlFor="password" className={classes["form-label"]}>
+            <span>Password</span>
+            </label>
+        </div>
+        <button type="submit" className={classes.input__btn}>
+          Login
+        </button>
+      </form>
+      <span className={classes.menu__footer}>
+        Already have an account? <Link to="/sign-up">Sign up</Link>
+      </span>
+    </div>
+  );
+};
 export default SignIn;
