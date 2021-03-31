e9c1e7f1468d600dee3b77095c2dd21dbb97d019
dron5 <dron5@mail.ru>
2021-03-31 11:15:52 +0700

    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,18 @@ import SignMenu from "../SignMenu/SignMenu";
 import UserMenu from "../UserMenu/UserMenu";
 import classes from "./Header.module.scss";
 
-const Header = () => (
-  <div className={classes.header}>
-    <div className={classes["header-container"]}>
-      <div className={classes["header-root"]}>
-        <Link to="/articles">Realworld Blog</Link>
+const Header = () => {
+  const authorized = false;
+  return (
+    <div className={classes.header}>
+      <div className={classes["header-container"]}>
+        <div className={classes["header-root"]}>
+          <Link to="/articles">Realworld Blog</Link>
+        </div>
+        {!authorized && <UserMenu />}
+        {!authorized && <SignMenu />}
       </div>
-      <UserMenu />
-      <SignMenu />
     </div>
-  </div>
-);
-
+  );
+};
 export default Header;
