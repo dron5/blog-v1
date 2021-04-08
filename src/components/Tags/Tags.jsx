3e86ef429a5530c6ffa5a0151256f00af5ee2be4
dron5 <dron5@mail.ru>
2021-04-08 15:19:12 +0700

    +a little bit of article

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,11 +1,34 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-array-index-key */
 import React from "react";
+import { Button } from "antd";
+
 import classes from "./Tags.module.scss";
 
 const Tags = () => {
-  console.log("in Tags");
-  return <div className={classes.container}>tagList</div>;
+  const onSubmit = () => {};
+  return(
+    <div className={classes.container}>
+      <form className={classes.form} onSubmit={onSubmit} >
+        <div className={classes["form-group"]}>
+          <input 
+            type="text"
+            name="tag"
+            className={classes["form-control"]}
+            placeholder=" "
+            id="tag"
+          />
+          <label htmlFor="tag" className={classes["form-label"]}>
+            <span>New tag</span>
+          </label>
+        </div>
+        <Button type="primary" className={classes["ant-btn-primary"]}>
+          Add tag
+        </Button>
+      </form>
+    </div>
+    
+  );
 };
 
 export default Tags;
