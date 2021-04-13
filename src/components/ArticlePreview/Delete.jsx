a4d4c4c619e75b47e9ad8865d46d98b37265623c
dron5 <dron5@mail.ru>
2021-04-13 19:21:09 +0700

    save

--- src/components/ArticlePreview/Delete.jsx
+++ src/components/ArticlePreview/Delete.jsx
@@ -5,12 +5,15 @@ import classes from "./ArticlePreview.module.scss";
 
 const Delete = () => {
   const [show, setShow] = useState(false);
-  const onClick = () => setShow(()=>!show);
+  const onDelet = () => setShow(()=>true);
+  const onConfirmYes = () => setShow(false);
+  const onConfirmNo = () => setShow(false);
+  console.log('show', show);
   return (
     <div className={classes["modal-wrapper"]}>
       <button 
         type="button"
-        onClick={onClick}
+        onClick={onDelet}
         className={[classes.btn, classes["btn-del"]].join(" ")}
         >
         Delete
@@ -20,10 +23,19 @@ const Delete = () => {
           ? `${classes.modal}`
           : `${classes.modal} ${classes.hidden}`
       }>
-        <span>A you sure?</span>
-        <div>
-          <button type="button">Yes</button>
-          <button type="button">No</button>
+        <div className={classes.delete__arrow} />
+        <span className={classes.delete__title}>A you sure to delete?</span>
+        <div className={classes["btn-box"]}>
+          <button 
+            type="button" 
+            onClick={onConfirmYes}
+            className={[classes.btn, classes["btn-confirm"]].join(' ')}>
+            Yes</button>
+          <button 
+            type="button"
+            onClick={onConfirmNo}
+            className={[classes.btn, classes["btn-confirm"]].join(' ')}>
+            No</button>
         </div>
       </div>
     </div>
