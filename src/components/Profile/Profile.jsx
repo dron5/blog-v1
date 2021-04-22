4eedff95f5bc0931a5373914dbe8a1ab3518bee0
dron5 <dron5@mail.ru>
2021-04-22 11:35:43 +0700

    form for profile was added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ import { connect } from "react-redux";
 import { updateUserRequest } from "../../services/asyncActions/asyncApi";
 import { getUserSelector } from "../../store/selectors";
 import * as fetch from "../../store/actions/usersActions";
-import classes from "./Profile.module.scss";
+import ProfileForm from "./ProfileForm";
 
 const EditProfile = ({ user, setUserAction }) => {
   const history = useHistory();
@@ -42,117 +42,11 @@ const EditProfile = ({ user, setUserAction }) => {
   const onSubmit = (data) => toUdateUser(data);
 
   return (
-    <div className={classes.container}>
-      <span className={classes.menu__title}>Edit Profile</span>
-      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={classes["form-group"]}>
-          <input
-            ref={register({
-              required: true,
-              minLength: {
-                value: 3,
-                message: "Username needs to be at least 3 characters",
-              },
-              maxLength: {
-                value: 20,
-                message: "Username needs to be not more than 20 characters",
-              },
-            })}
-            type="text"
-            name="username"
-            className={
-              !errors.username
-                ? `${classes["form-control"]}`
-                : [classes["form-control"], classes["alert-border"]].join(" ")
-            }
-            placeholder="your username"
-            id="username"
-          />
-          <label htmlFor="username" className={classes["form-label"]}>
-            <span>Username</span>
-          </label>
-          {errors.username && (
-            <p className={classes["alert-message"]}>
-              {errors.username.message}
-            </p>
-          )}
-        </div>
-        <div className={classes["form-group"]}>
-          <input
-            ref={register({
-              required: true,
-              pattern: {
-                value: /^.+@.+\..+$/,
-                message: "Enter valid email, please",
-              },
-            })}
-            type="email"
-            name="email"
-            className={
-              !errors.email
-                ? `${classes["form-control"]}`
-                : `${classes["form-control"]} ${classes["alert-border"]}`
-            }
-            placeholder=" "
-            id="email"
-          />
-          <label htmlFor="email" className={classes["form-label"]}>
-            <span>Email address</span>
-          </label>
-          {errors.email && (
-            <p className={classes["alert-message"]}>{errors.email.message}</p>
-          )}
-        </div>
-        <div className={classes["form-group"]}>
-          <input
-            ref={register({
-              required: true,
-              minLength: {
-                value: 8,
-                message: "Your password needs to be at list 8 char",
-              },
-              maxLength: {
-                value: 20,
-                message: "Your password needs to be not more 40 char",
-              },
-            })}
-            type="password"
-            name="password"
-            className={
-              !errors.password
-                ? `${classes["form-control"]}`
-                : `${classes["form-control"]} ${classes["alert-border"]}`
-            }
-            placeholder=" "
-            id="password"
-          />
-          <label htmlFor="password" className={classes["form-label"]}>
-            <span>New password</span>
-          </label>
-          {errors.password && (
-            <p className={classes["alert-message"]}>
-              {errors.password.message}
-            </p>
-          )}
-        </div>
-        <div className={classes["form-group"]}>
-          <input
-            ref={register({ required: false })}
-            type="url"
-            name="avatar"
-            className={classes["form-control"]}
-            placeholder=" "
-            id="avatar"
-          />
-          <label htmlFor="avatar" className={classes["form-label"]}>
-            <span>Avatar mage (url)</span>
-          </label>
-        </div>
-        <button type="submit" className={classes.input__btn}>
-          Save
-        </button>
-      </form>
-    </div>
+    <ProfileForm
+      register={register}
+      errors={errors}
+      handleSubmit={handleSubmit(onSubmit)}
+    />
   );
 };
 
